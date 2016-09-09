5f19426dfbb202d07f3a6dc46df03bf9d3647afe
toledoJavier <toledojavier90@gmail.com>
2016-09-09 20:35:27 -0300

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the webFrontendApp
+ */
+angular.module('webFrontendApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
